3d40361b09506e62cc4527c318cdcc8d6ddc929b
John FitzGerald <iconium9000@gmail.com>
2016-10-25 21:01:50 -0700

    Update mazeGame.js

--- mazeGame.js
+++ mazeGame.js
@@ -1004,9 +1004,11 @@ Level.prototype.getTarget = function(p) {
     })
 }
 Level.prototype.setTarget = function(p) {
-    if (this.finalPoint.dist(p)<this.radius &&  Game.lvl.next &&  (this.isUnlocked || devMode){
-        Game.lvl = Game.lvl.next
-    } 
+    if (this.finalPoint.dist(p)<this.radius && Game.lvl.next && (devMode || Game.lvl.val.isUnlocked)) {
+         Game.lvl = Game.lvl.next
+         return
+    }  
+
     ++this.score
     var tar = this.getTarget(p)
     if (this.sel == null ) {
